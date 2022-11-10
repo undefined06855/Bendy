0bcefa6c77dc691c5976041923a40766fe899a8e
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2022-11-10 21:49:02 +0000

    added zoom slider

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // yes this is built on wordToShape...
 
-const version = 0.6
+const version = 0.7
 
     , canvas = document.querySelector("canvas")
     , ctx = canvas.getContext("2d")
@@ -12,9 +12,6 @@ const version = 0.6
         [5, 20, 100, 5, 15],
         [10, 500, 500, 30, 30],
     ]
-
-
-
 
 var labelOffset = 2
   , max = 0
@@ -39,6 +36,7 @@ file.line.length = 4
 file.line.change = .8
 file.line.dist = 50
 file.line.size = 2
+file.line.zoom = 1
 
 file.other.secretFound = false
 file.other.label = true
@@ -50,75 +48,6 @@ canvas.addEventListener("click", () => {
     document.getElementById("secretBox").style.scale = "1"
 })
 
-function downloadFile(data, filename, type) {
-    var file = new Blob([data], {type: type});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var a = document.createElement("a"),
-                url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function() {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);  
-        }, 0); 
-    }
-}
-
-function exportSettings()
-{
-    filename = document.getElementById("filenameInput").value.replace(".bline", "") + ".bline"
-
-    //https://stackoverflow.com/a/30832210
-    var file2 = JSON.stringify(file)
-    var f = new Blob([file2], {type: "text/json"});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(f, filename);
-    else { // Others
-        var a = document.createElement("a"),
-                url = URL.createObjectURL(f)
-        a.href = url
-        a.download = filename
-        document.body.appendChild(a)
-        a.click()
-        setTimeout(function() {
-            document.body.removeChild(a)
-            window.URL.revokeObjectURL(url)
-        }, 0)
-    }
-    console.log(file)
-}
-
-function importSettings(importer)
-{
-    var f = importer.target.files[0]
-    if (!f) return
-    var reader = new FileReader()
-    reader.onload = e => {
-        file = JSON.parse(e.target.result)
-
-        document.getElementById("scale").value = file.line.speed
-        document.getElementById("scale2").value = file.line.length
-        document.getElementById("scale4").value = file.line.distance
-        document.getElementById("scale3").value = file.line.change
-        document.getElementById("scale5").value = file.line.size
-
-        document.getElementById("scaleLabel").innerText = `Speed (${document.getElementById("scale").value}): `
-        document.getElementById("scaleLabel2").innerText = `Length (${document.getElementById("scale2").value}): `
-        document.getElementById("scaleLabel4").innerText = `Distance (${document.getElementById("scale4").value}): `
-        document.getElementById("scaleLabel3").innerText = `Change (${document.getElementById("scale3").value}): `
-        document.getElementById("scaleLabel5").innerText = `Size (${document.getElementById("scale5").value}): `
-
-        document.getElementById("labelCh").checked = file.other.label
-        document.getElementById("linesCh").checked = file.other.lines
-        document.getElementById("largerCh").checked = file.other.secretFound
-    }
-    reader.readAsText(f)
-}
-
 document.getElementById("fileInput").addEventListener("change", importSettings, false);
 
 // frame
@@ -126,11 +55,12 @@ function dp()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    speed = file.line.speed
-    length = file.line.length
-    change = file.line.change
-    dist = file.line.dist
-    size = file.line.size
+    speed = file.line.speed    || 1.25
+    length = file.line.length  || 4
+    change = file.line.change  || 0.8
+    dist = file.line.dist      || 50
+    size = file.line.size      || 2
+    zoom = file.line.zoom      || 1
 
     pl = []
     for (var i = 0; i < length; i++)
@@ -172,6 +102,17 @@ document.getElementById("scale3").addEventListener("input", () => {
 document.getElementById("scale5").addEventListener("input", () => {
     file.line.size = Number(document.getElementById("scale5").value)
     document.getElementById("scaleLabel5").innerText = `Size (${document.getElementById("scale5").value}): `
+})
+
+document.getElementById("zoom").addEventListener("input", () => {
+    file.line.zoom = Number(document.getElementById("zoom").value)
+    document.getElementById("zoomLabel").innerText = `Zoom (${(1/document.getElementById("zoom").value).toFixed(2)}): `
+
+    canvas.width = 500 * file.line.zoom
+    canvas.height = 500 * file.line.zoom
+
+    center.x = canvas.width / 2
+    center.y = canvas.height / 2
 })
 
 requestAnimationFrame(dp)
